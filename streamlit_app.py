b7741ba6e941246da8fcbeb3bde84dd6dde81824
Supersauen <115140464+Supersauen@users.noreply.github.com>
2022-10-07 13:43:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,5 @@ import numpy as np
 import pandas as pd
 
 
-
-df = pd.read_txt("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt")
-
-print(df)
+print(http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt)
 
