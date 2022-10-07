faf8175a630df8097cca9f2349b3035c1a746d31
Supersauen <115140464+Supersauen@users.noreply.github.com>
2022-10-07 13:36:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,7 @@ Here's our first attempt at using data to create a table:
 """
 import streamlit as st
 import numpy as np
-import pandas as pd
-import pickle
-import time
-from matplotlib import pyplot as plt
-from  matplotlib.ticker import FuncFormatter
-import seaborn as sns
+
 
 
 df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt")
