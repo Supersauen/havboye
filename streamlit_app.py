05ae8ad99aecfd24b86611ccab3b08f84269e1e7
Supersauen <115140464+Supersauen@users.noreply.github.com>
2022-10-17 21:06:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ headers = []
 #ping http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt
 #print(sensor.marin.ntnu.no/logs/Ulstein05_10.txt)c
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'long','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a11','a13'])
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'long','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
 
 st.bar_chart(df)
 
