daa9d32e97ebda1f18a13b0e7acefd44ea9a31db
Supersauen <115140464+Supersauen@users.noreply.github.com>
2022-10-17 21:02:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,12 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+headers = []
+
 #ping http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt
 #print(sensor.marin.ntnu.no/logs/Ulstein05_10.txt)c
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt")
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt", headers = {'filename', 'power','loc', 'time', 'lat', 'long','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a11','a13'})
 
 st.bar_chart(df)
 
