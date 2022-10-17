bac2c297d30959d1fad4cba9c0ae6ef23d504e6f
Supersauen <115140464+Supersauen@users.noreply.github.com>
2022-10-17 21:12:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ headers = []
 
 df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'long','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
 
-st.map(df[['time','lat','long']])
+st.bar_chart(df[['time','lat','long']])
 
 
 st.text("Test")
