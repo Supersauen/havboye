9278984eb5584c2b1cd53c11b0c9df266206f6fd
Supersauen <115140464+Supersauen@users.noreply.github.com>
2022-10-17 20:56:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,12 @@ import numpy as np
 import pandas as pd
 
 #ping http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt
-#print(sensor.marin.ntnu.no/logs/Ulstein05_10.txt)
+#print(sensor.marin.ntnu.no/logs/Ulstein05_10.txt)c
+
+df = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein05_10.txt")
+
+df.plot()
+
+st.bar_chart(df)
 
 st.text("Test")
