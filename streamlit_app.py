70135dc477d4cca7ad7481af7ab4d16a53a9d3fa
Supersauen <115140464+Supersauen@users.noreply.github.com>
2022-10-18 13:34:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,20 @@ import pandas as pd
 headers = []
 
 
-df = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+df4 = pd.read_csv("http://sensor.marin.ntnu.no/logs/UlsteinGr4.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+df2 = pd.read_csv("http://sensor.marin.ntnu.no/logs/Ulstein17_10.txt", names = ['filename', 'power','loc', 'time', 'lat', 'lon','a1','a2','a3','a4','a5','a6','a7','a8','a9','a10','a11','a12','a13'])
+
 st.header("Bøye nr 4 test")
 #st.bar_chart(df[['lat','lon']])
-st.dataframe(df)
+st.dataframe(df4)
+st.map(df4)
 
-st.map(df)
+st.header("Bøye nr 2 test")
+#st.bar_chart(df2[['lat','lon']])
+st.dataframe(df4)
+st.map(df2)
 
-st.text("Test")
+
+
+
+#st.text("Test")
